932aa0277cdcd45103b2d96a7b766a8261de0d42
Your Name <dev.hashebur@gmail.com>
2024-06-03 10:57:12 +0600

    server update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.use(bodyParser.json({ limit: "500mb" }));
 
 // Set up multer for file upload
